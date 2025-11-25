e2042947e8a68cc1a00267963cd235fbff9a8ab3
Kento182 <kento.182@hotmail.com>
2025-11-25 12:50:08 -0600

    Sección 1 del curso JS Moderno

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,6 @@
+let nombre = prompt('Cual es tu nombre?');
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
+
+let producto = 'Monitor 24 pulgadas';
