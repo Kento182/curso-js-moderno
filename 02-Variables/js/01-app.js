3eb4ac06d5e7ed88a5aff741e688a176285704e3
Kento182 <kento.182@hotmail.com>
2025-11-25 17:37:19 -0600

    Sección 2 del curso JS Moderno

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,25 @@
+// Inicializar una variable con un valor
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+// JavaScript es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+
+// Se pueden inicializar sin valor y asignarlo después
+var disponible;
+disponible = true;
+
+disponible = false;
+
+// Inicializar múltiples variables
+var categoria = "Computadoras",
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+// Las variables no pueden iniciar con números ni contener espacios
---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,11 @@
+const producto = 'Tablet';
+
+// Son CONSTANTES, no se pueden reasignar
+// siempre se deben inicializar con un valor
+
+console.log(producto);
+
+
+const precio = 20;
+
+console.log(precio);
