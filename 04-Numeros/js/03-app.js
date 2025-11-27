d1390cb9bede142d1358d1c77fc2c93d76c53b8e
Kento182 <kento.182@hotmail.com>
2025-11-27 11:21:41 -0600

    Sección 4 del curso JS Moderno

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,37 @@
+let resultado;
+
+// PI
+resultado = Math.PI;
+
+// Redondear
+resultado = Math.round(2.8);
+resultado = Math.round(2.2);
+
+// Redondear hacia arriba
+resultado = Math.ceil(2.1);
+
+// Redondear hacia abajo
+resultado = Math.floor(2.9);
+
+// Raíz cuadrada
+resultado = Math.sqrt(144);
+
+// Absoluto
+resultado = Math.abs(-10);
+
+// Potencia
+resultado = Math.pow(8,3);
+
+// Minimo
+resultado = Math.min(3,5,1);
+
+// Maximo
+resultado = Math.max(3,5,1);
+
+// Aleatorio
+resultado = Math.random()*100;
+
+// Aleatorio dentro de un rango...
+resultado = Math.floor(Math.random()*100);
+
+console.log(resultado);
---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,10 @@
+let puntaje = 5;
+let puntaje2 = 5;
+
+puntaje++; // 5
+++puntaje; // 6
+puntaje2--; // 5
+--puntaje2; // 4
+
+console.log(puntaje);
+console.log(puntaje2);
